af04fa67e7b9f84a5ed9e2fd97b0922624e54210
oyesa <mercyoyesa2018@gmail.com>
2022-07-19 22:45:53 +0300

    create views for home and profile

--- mtaaapp/models.py
+++ mtaaapp/models.py
@@ -1,6 +1,3 @@
-from email.mime import image
-from socket import CAN_SFF_MASK
-from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
--- mtaaapp/views.py
+++ mtaaapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import *
+
+
 
 # Create your views here.
+def home(request):
+  current_user=request.user
+  profiles=Profile.objects.filter(id=current_user.id).all()
+  return render(request, 'home.html')
+
+
+def profile(request):
+  current_user=request.user
+  return render(request, 'profile.html')
