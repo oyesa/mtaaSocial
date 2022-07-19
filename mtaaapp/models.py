58d5db1ab419ecb15f5f4929c895cee688c540a9
oyesa <mercyoyesa2018@gmail.com>
2022-07-19 22:18:24 +0300

    create image, profile and comment models

--- mtaaapp/models.py
+++ mtaaapp/models.py
@@ -1,3 +1,25 @@
+from email.mime import image
+from socket import CAN_SFF_MASK
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
 
 # Create your models here.
+class Image(models.Model):
+  user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
+  image=CloudinaryField('image')
+  image_desc=models.TextField()
+  profile=models.ForeignKey(User, on_delete=models.CASCADE
+  )
+
+class Profile(models.Model):
+  user=models.OneToOneField(User, on_delete=models.CASCADE)
+  prof_pic=CloudinaryField('image')
+  bio=models.TextField()
+
+class Comments(models.Model):
+  user=models.ForeignKey(User, on_delete=models.CASCADE)
+  comment=models.CharField(max_length=400)
